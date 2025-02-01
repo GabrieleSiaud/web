0581c6eb6c773933581dc104eb190b03592873bc
GabrieleSiaud <168407721+GabrieleSiaud@users.noreply.github.com>
2025-02-01 23:15:12 +0200

    Add files via upload

---
+++ helloworld/app.js
@@ -0,0 +1,10 @@
+var container = document.querySelector('.container');
+var heading = document.createElement('h1');
+heading.textContent = 'Myliuuuu tave ❤️';
+var image = document.createElement('img');
+image.src = 'images/aretas.jpg';
+image.alt = 'Aretas Picture';
+image.width = 300;
+container.appendChild(heading);
+container.appendChild(image);
+document.body.appendChild(heading);
